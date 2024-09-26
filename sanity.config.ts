e14a122a124453d3db9deb06e2c97d2aff96c600
Dag Stuan <dag.stuan@bekk.no>
2024-09-26 22:26:59 +0200

    Structure first.

--- sanity.config.ts
+++ sanity.config.ts
@@ -21,6 +21,7 @@ export default defineConfig({
   // Add and edit the content schema in the './sanity/schemaTypes' folder
   schema,
   plugins: [
+    structureTool({ structure }),
     presentationTool({
       previewUrl: {
         previewMode: {
@@ -28,7 +29,6 @@ export default defineConfig({
         },
       },
     }),
-    structureTool({ structure }),
     // Vision is for querying with GROQ from inside the Studio
     // https://www.sanity.io/docs/the-vision-plugin
     visionTool({ defaultApiVersion: apiVersion }),
