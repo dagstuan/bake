c803bb41068a9a5cfcbd8fe53465cb1b8b0c1d0d
Dag Stuan <dag.stuan@bekk.no>
2024-09-29 15:37:45 +0200

    Fix hydration errors with wakelock

--- src/components/Recipe/WakeLockToggle.tsx
+++ src/components/Recipe/WakeLockToggle.tsx
@@ -5,17 +5,20 @@ import { useWakeLock } from "react-screen-wake-lock";
 import { Switch } from "../ui/switch";
 import { Label } from "../ui/label";
 
+const isServer = typeof window === "undefined";
+
 export const WakeLockToggle = (): JSX.Element | null => {
   const [checked, setChecked] = useState(false);
   const { isSupported, request, release } = useWakeLock({
     onRelease: () => setChecked(false),
   });
 
-  return isSupported ? (
+  return (
     <div className="flex items-center space-x-3">
       <Label htmlFor="wake-lock-toggle">Behold skjermen på</Label>
       <Switch
         id="wake-lock-toggle"
+        disabled={!isServer && !isSupported}
         checked={checked}
         onCheckedChange={(checked) => {
           setChecked(checked);
@@ -27,5 +30,5 @@ export const WakeLockToggle = (): JSX.Element | null => {
         }}
       />
     </div>
-  ) : null;
+  );
 };
