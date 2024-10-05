45e0a2a409217f6553a9b05026d1b4b758882235
Dag Stuan <dag.stuan@bekk.no>
2024-10-05 09:56:43 +0200

    Max width for nav

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -3,17 +3,19 @@ import { TypographyLink } from "../Typography/TypographyLink";
 
 export const Nav = () => {
   return (
-    <nav className="flex w-full items-center justify-between border-b border-b-gray-200 bg-accent px-6 py-4 text-accent-foreground sm:py-5">
-      <Link href="/" className="text-4xl">
-        🍞
-      </Link>
-      <ul>
-        <li>
-          <TypographyLink type="internal" href="/oppskrifter">
-            Alle oppskrifter
-          </TypographyLink>
-        </li>
-      </ul>
+    <nav className="border-b border-b-gray-200 bg-accent px-6">
+      <div className="mx-auto flex w-full max-w-6xl items-center justify-between py-4 text-accent-foreground sm:py-5">
+        <Link href="/" className="text-4xl">
+          🍞
+        </Link>
+        <ul>
+          <li>
+            <TypographyLink type="internal" href="/oppskrifter">
+              Alle oppskrifter
+            </TypographyLink>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 };
