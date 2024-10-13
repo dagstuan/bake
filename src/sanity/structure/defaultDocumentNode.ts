01e1226903aa951c1cbca71a6161becc233cfe54
Dag Stuan <dag.stuan@bekk.no>
2024-10-13 21:59:08 +0200

    Add documents pane plugin. Show recipe usage for other types.

--- src/sanity/structure/defaultDocumentNode.ts
+++ src/sanity/structure/defaultDocumentNode.ts
@@ -10,6 +10,10 @@ import { recipeType } from "../schemaTypes/recipe/recipeType";
 import { DocumentIcon } from "@sanity/icons";
 import { aboutType } from "../schemaTypes/singletons/aboutType";
 import { homeType } from "../schemaTypes/singletons/homeType";
+import { ingredientType } from "../schemaTypes/ingredientType";
+import DocumentsPane from "sanity-plugin-documents-pane";
+import { recipeIngredientType } from "../schemaTypes/recipe/recipeIngredientType";
+import { categoryType } from "../schemaTypes/categoryType";
 
 const createIframeOptions = (S: StructureContext): IframeOptions => ({
   url: {
@@ -45,6 +49,46 @@ export const defaultDocumentNode: DefaultDocumentNodeResolver = (
           .icon(EyeOpenIcon)
           .options(createIframeOptions(S.context))
           .title("Preview"),
+      ]);
+    case ingredientType.name:
+      return S.document().views([
+        S.view.form().icon(DocumentIcon),
+        S.view
+          .component(DocumentsPane)
+          .icon(() => "📜")
+          .options({
+            query:
+              '*[_type == "recipe" && count((ingredients[]._ref)[@ in *[references($id)]._id]) > 0]',
+            params: { id: `_id` },
+            options: { perspective: "previewDrafts" },
+          })
+          .title("Used in recipes"),
+      ]);
+    case recipeIngredientType.name:
+      return S.document().views([
+        S.view.form().icon(DocumentIcon),
+        S.view
+          .component(DocumentsPane)
+          .icon(() => "📜")
+          .options({
+            query: "*[references($id)]",
+            params: { id: `_id` },
+            options: { perspective: "previewDrafts" },
+          })
+          .title("Used in recipes"),
+      ]);
+    case categoryType.name:
+      return S.document().views([
+        S.view.form().icon(DocumentIcon),
+        S.view
+          .component(DocumentsPane)
+          .icon(() => "📜")
+          .options({
+            query: '*[_type == "recipe" && references($id)]',
+            params: { id: `_id` },
+            options: { perspective: "previewDrafts" },
+          })
+          .title("Recipes"),
       ]);
     default:
       return S.document().views([S.view.form().icon(DocumentIcon)]);
