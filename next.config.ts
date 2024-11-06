93d8ce5a4b6319e2e298ac9edee60e9ed9f03218
Dag Stuan <dag.stuan@bekk.no>
2024-11-06 22:47:41 +0100

    Enable tree shaking for sentry.

--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,19 @@ const nextConfig: NextConfig = {
     // Matches the behavior of `sanity dev` which sets styled-components to use the fastest way of inserting CSS rules in both dev and production. It's default behavior is to disable it in dev mode.
     SC_DISABLE_SPEEDY: "false",
   },
+  webpack: (config, { webpack }) => {
+    config.plugins.push(
+      new webpack.DefinePlugin({
+        __SENTRY_DEBUG__: false,
+        __RRWEB_EXCLUDE_IFRAME__: true,
+        __RRWEB_EXCLUDE_SHADOW_DOM__: true,
+        __SENTRY_EXCLUDEgs_REPLAY_WORKER__: true,
+      }),
+    );
+
+    // return the modified config
+    return config;
+  },
 };
 
 const withBundleAnalyzer = nextBundleAnalyzer({
