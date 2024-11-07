6dc620f11cbb9caebc320d96085c8a1f26632dde
Dag Stuan <dag.stuan@bekk.no>
2024-11-07 13:05:56 +0100

    Capture console errors everywhere.

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -5,4 +5,5 @@ Sentry.init({
   dsn: "https://0fb330db3a98b6acb34139db0f4e5516@o4508195146694656.ingest.de.sentry.io/4508195147939920",
   tracesSampleRate: 0.5,
   debug: false,
+  integrations: [Sentry.captureConsoleIntegration({ levels: ["error"] })],
 });
