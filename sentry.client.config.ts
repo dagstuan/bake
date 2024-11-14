c44e851842599c85b21a6d01ef60427ffe3311b5
Dag Stuan <dag.stuan@bekk.no>
2024-11-14 13:12:48 +0100

    Remove sentry due to large bundle sizes.

--- sentry.client.config.ts
+++
@@ -1,9 +0,0 @@
-import { init } from "@sentry/nextjs";
-
-init({
-  enabled: process.env.NODE_ENV !== "development",
-
-  dsn: "https://0fb330db3a98b6acb34139db0f4e5516@o4508195146694656.ingest.de.sentry.io/4508195147939920",
-  tracesSampleRate: 0.5,
-  debug: false,
-});
