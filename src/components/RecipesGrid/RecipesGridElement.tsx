9c11523df86c16a8992393e7dc937cf69af7d41b
Dag Stuan <dag.stuan@bekk.no>
2024-11-20 22:25:44 +0100

    Stegaclean recipe grid elements.

--- src/components/RecipesGrid/RecipesGridElement.tsx
+++ src/components/RecipesGrid/RecipesGridElement.tsx
@@ -7,6 +7,7 @@ import { RecipesListQueryResult } from "../../../sanity.types";
 import { ArrayElement } from "@/utils/types";
 import { formatDurationShort } from "@/utils/durationUtils";
 import { cn } from "@/lib/utils";
+import { stegaClean } from "next-sanity";
 
 const gridImageWidth = 360;
 const gridImageHeight = 202;
@@ -49,7 +50,7 @@ export const RecipesGridElement = ({
                 .url() ?? ""
             }
             priority={prority}
-            alt={mainImage?.alt ?? "Recipe"}
+            alt={stegaClean(mainImage?.alt ?? "Recipe")}
             className="aspect-video w-full rounded-t-lg object-cover"
             blurDataURL={mainImage?.asset?.metadata?.lqip ?? undefined}
             sizes="(max-width: 640px) 100vw, (max-width: 1024px) 50vw, 30vw"
@@ -60,7 +61,7 @@ export const RecipesGridElement = ({
           </div>
         )}
         <div className="flex justify-between p-4">
-          <h2 className="text-xl font-semibold">{title}</h2>
+          <h2 className="text-xl font-semibold">{stegaClean(title)}</h2>
           {duration && (
             <div className="flex items-center gap-1 text-sm text-muted-foreground">
               <ClockIcon /> {duration}
