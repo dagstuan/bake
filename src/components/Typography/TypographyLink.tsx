e59daf49b8151919f2793ad536489aac1c2c20f2
Dag Stuan <dag.stuan@bekk.no>
2024-11-22 16:55:57 +0100

    Active style for links.

--- src/components/Typography/TypographyLink.tsx
+++ src/components/Typography/TypographyLink.tsx
@@ -27,7 +27,7 @@ export const TypographyLink = ({
     <Elem
       href={href}
       className={cn(
-        "font-medium text-primary underline-offset-4 hover:text-secondary-foreground hover:underline",
+        "font-medium text-primary underline-offset-4 hover:text-secondary-foreground hover:underline active:no-underline",
         className,
       )}
     >
