7dbe535d698a32a90f7d165d6d0bd1d1e8e9d5b6
Dag Stuan <dag.stuan@bekk.no>
2024-12-02 16:55:54 +0100

    re-add maxDuration

--- src/app/(studio)/studio/[[...tool]]/page.tsx
+++ src/app/(studio)/studio/[[...tool]]/page.tsx
@@ -2,6 +2,8 @@ import { NextStudio } from "next-sanity/studio";
 import config from "../../../../../sanity.config";
 
 export const dynamic = "force-static";
+export const revalidate = false;
+export const maxDuration = 60;
 
 export { metadata, viewport } from "next-sanity/studio";
 
