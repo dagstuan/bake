edaa48f84e302f720c56d5d8e08e3db27082240a
Dag Stuan <dag.stuan@bekk.no>
2024-12-02 16:37:04 +0100

    Attempt to remove maxDuration.

--- src/app/(studio)/studio/[[...tool]]/page.tsx
+++ src/app/(studio)/studio/[[...tool]]/page.tsx
@@ -1,7 +1,6 @@
 import { NextStudio } from "next-sanity/studio";
 import config from "../../../../../sanity.config";
 
-export const maxDuration = 60;
 export const dynamic = "force-static";
 
 export { metadata, viewport } from "next-sanity/studio";
