c4e1dbf05ff55d3de75bb444353dce1238e4ea5a
Dag Stuan <dag.stuan@bekk.no>
2025-10-19 16:54:57 +0200

    Add ingredients to json+ld

--- src/components/pages/RecipePage/RecipePage.tsx
+++ src/components/pages/RecipePage/RecipePage.tsx
@@ -13,12 +13,70 @@ interface RecipePageProps {
   data: RecipeQueryResult | null;
 }
 
+const extractIngredients = (
+  recipe: NonNullable<RecipeQueryResult>,
+): string[] => {
+  const ingredients: string[] = [];
+
+  recipe.ingredients?.forEach((item) => {
+    if (item._type === "reference" && item.ingredient) {
+      const ing = item.ingredient;
+      let text = "";
+
+      if (ing._type === "ingredient" && ing.name) {
+        // Format: amount unit ingredient (comment)
+        if (item.percent && recipe.baseDryIngredients) {
+          const amount = (item.percent / 100) * recipe.baseDryIngredients;
+          text = `${Math.round(amount)} ${item.unit ?? "g"} ${ing.name}`;
+        } else {
+          text = ing.name;
+        }
+
+        if (item.comment) {
+          text += ` (${item.comment})`;
+        }
+
+        ingredients.push(text);
+      } else if (ing._type === "recipe" && ing.title) {
+        ingredients.push(ing.title);
+      }
+    } else if (item._type === "ingredientGroup" && item.ingredients) {
+      // Add ingredients from groups
+      item.ingredients.forEach((groupIng) => {
+        const ing = groupIng.ingredient;
+        let text = "";
+
+        if (ing?._type === "ingredient" && ing.name) {
+          if (groupIng.percent && recipe.baseDryIngredients) {
+            const amount = (groupIng.percent / 100) * recipe.baseDryIngredients;
+            text = `${Math.round(amount)} ${groupIng.unit ?? "g"} ${ing.name}`;
+          } else {
+            text = ing.name;
+          }
+
+          if (groupIng.comment) {
+            text += ` (${groupIng.comment})`;
+          }
+
+          ingredients.push(text);
+        } else if (ing?._type === "recipe" && ing.title) {
+          ingredients.push(ing.title);
+        }
+      });
+    }
+  });
+
+  return ingredients;
+};
+
 export const RecipePage = (props: RecipePageProps) => {
   const { params, data: recipe } = props;
 
   if (!recipe) {
     return notFound();
   }
+
+  const recipeIngredients = extractIngredients(recipe);
 
   const jsonLd: WithContext<RecipeSchema> = {
     "@context": "https://schema.org",
@@ -43,6 +101,7 @@ export const RecipePage = (props: RecipePageProps) => {
       : undefined,
     recipeCategory:
       recipe.categories?.map((category) => category.title).join(", ") ?? "",
+    recipeYield: recipe.servings?.toString(),
     image: recipe.mainImage
       ? (urlForImage(recipe.mainImage)
           ?.width(800)
@@ -51,6 +110,8 @@ export const RecipePage = (props: RecipePageProps) => {
           .dpr(1)
           .url() ?? "")
       : "",
+    recipeIngredient:
+      recipeIngredients.length > 0 ? recipeIngredients : undefined,
   };
 
   return (
